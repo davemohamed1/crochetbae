8f09eaca728ae2b35383ac09d4a158159d60a0a7
Abdolkarim Mohamed <abdolkarim.mohamed123@gmail.com>
2025-11-11 18:00:15 -0800

    website

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily: 'system-ui, sans-serif', padding: 24}}>
+      <h1>CrochetBae</h1>
+      <p>Deployed with GitHub Pages.</p>
+    </div>
+  )
+}
