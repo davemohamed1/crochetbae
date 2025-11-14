70def90a5e6c540954fb56a94e546cab3896dd59
Karim Mohamed <147700308+davemohamed1@users.noreply.github.com>
2025-11-14 00:41:20 -0800

    deployed to firebase
    next implement fire store and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CounterPage from './pages/CounterPage';
-import AboutPage from './pages/AboutPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CounterPage from "./pages/CounterPage";
+import AboutPage from "./pages/AboutPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <Router basename="/crochetbae">
+    <Router>
       <Routes>
         <Route path="/" element={<CounterPage />} />
         <Route path="/about" element={<AboutPage />} />
